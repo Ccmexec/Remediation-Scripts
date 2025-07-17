10947d58b8a9fed565afb5e3977c435e6d880617
Ryan Malayter <malayter@gmail.com>
2025-07-17 16:12:41 -0500

    Update ResetWindowsUpdateRemediation.ps1
    add removal of qmgr*.dat files based on Microsoft guidance at https://learn.microsoft.com/en-us/troubleshoot/windows-client/installing-updates-features-roles/additional-resources-for-windows-update#how-do-i-reset-windows-update-components

--- ResetWindowsUpdateRemediation.ps1
+++ ResetWindowsUpdateRemediation.ps1
@@ -3,7 +3,8 @@ Version: 1.0
 Author: 
 - JOrgen Nilsson (ccmexec.com)
 Script: ResetWindowsUpdateRemediation.ps1
-Description:
+Description: reset windows update services based on Microsoft guidance at 
+  https://learn.microsoft.com/en-us/troubleshoot/windows-client/installing-updates-features-roles/additional-resources-for-windows-update#how-do-i-reset-windows-update-components
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
 Version 1.0: Init
 Run as: Admin
@@ -14,6 +15,11 @@ if ($DependentService) {Stop-Service $DependentService -Force}
 Stop-Service -Name wuauserv 
 Stop-Service -Name cryptsvc -Force
 Stop-Service -Name bits -Force
+
+#delete qmgr*.dat files based on Micorosft guidance 
+if (Test-Path "$env:ALLUSERSPROFILE\Microsoft\Network\Downloader\qmgr*.dat") {
+    Remove-Item "$env:ALLUSERSPROFILE\Microsoft\Network\Downloader\qmgr*.dat" -Force
+}
 
 if (Test-Path $Env:Windir\SoftwareDistribution.bak) {
     Remove-Item $Env:Windir\SoftwareDistribution.bak -Recurse -Force
